505bcc56e99b0ca185db5cec80a51ed3192026c3
Rémi Weng <ixartz@users.noreply.github.com>
2021-07-01 23:50:07 +0200

    Remove unused npm-run-all and format handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -4,9 +4,11 @@ import serverlessHttp from 'serverless-http';
 const app = express();
 app.use(json());
 
-app.get('/', (_, res) => res.json({
-  msg: 'Hello World',
-}));
+app.get('/', (_, res) =>
+  res.json({
+    msg: 'Hello World',
+  })
+);
 
 app.use((_, res, _2) => {
   res.status(404).json({ error: 'NOT FOUND' });
