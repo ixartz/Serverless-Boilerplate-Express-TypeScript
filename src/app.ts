03776dac703221eb7d4e3dca2df8f6a9da5ea1d9
Rémi Weng <ixartz@users.noreply.github.com>
2022-04-29 03:30:58 +0200

    test: add supertest for testing Express routes

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { json } from 'express';
+import helmet from 'helmet';
+
+const app = express();
+app.use(json());
+app.use(helmet());
+
+app.get('/', (_, res) =>
+  res.json({
+    msg: 'Hello World',
+  })
+);
+
+app.use((_, res, _2) => {
+  res.status(404).json({ error: 'NOT FOUND' });
+});
+
+export { app };
