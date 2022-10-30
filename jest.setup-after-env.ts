f75391eca8800e75147624f0150f5e55b68dd42b
Rémi Weng <ixartz@users.noreply.github.com>
2022-10-30 22:31:52 +0100

    feat: remove data between each tests

--- jest.setup-after-env.ts
+++ jest.setup-after-env.ts
@@ -1,4 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import type { Prisma, PrismaPromise } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { execa } from 'execa';
 import pRetry from 'p-retry';
 
@@ -16,6 +18,28 @@ const prismaDbPush = async () => {
   );
 };
 
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.MOCK_MONGODB_DATABASE_ENDPOINT,
+    },
+  },
+});
+
 beforeAll(async () => {
   await pRetry(prismaDbPush, { retries: 5 });
+});
+
+afterEach(async () => {
+  const deleteList: PrismaPromise<Prisma.BatchPayload>[] = [];
+
+  Object.getOwnPropertyNames(prisma).forEach((elt) => {
+    if (!elt.startsWith('_') && !elt.startsWith('$')) {
+      // TODO: Remove the ts-ignore
+      // @ts-ignore
+      deleteList.push(prisma[elt].deleteMany({}));
+    }
+  });
+
+  await Promise.all(deleteList);
 });
